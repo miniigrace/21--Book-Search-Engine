b294863ac95285f93fe48d73a60272a59e0c3cc8
Grace Ocampina <grace.ocampina@ucsf.edu>
2023-03-05 19:50:05 -0800

    updated codes, run develops and fail

--- server/server.js
+++ server/server.js
@@ -7,30 +7,37 @@ const { authMiddleware } = require('./utils/auth');
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
+async function startServer(typeDefs, resolvers){
+  await server.start()
 server.applyMiddleware({ app });
+}
+//startServer()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
+  
 });
+
+startServer(typeDefs, resolvers)
